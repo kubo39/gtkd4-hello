c8b10616b0d1b897e6e12452961df65215c968b0
Hiroki Noda <kubo39@gmail.com>
2021-05-27 01:33:42 +0900

    Remove Box

--- source/app.d
+++ source/app.d
@@ -1,29 +1,24 @@
 import gio.Application : GioApplication = Application;
 import gtk.Application;
 import gtk.ApplicationWindow;
-import gtk.Box;
 import gtk.Button;
 
 int main(string[] args)
 {
     auto application = new Application("org.gtk.example", GApplicationFlags.FLAGS_NONE);
-    application.addOnActivate((GioApplication _) {
+    application.addOnActivate((GioApplication) {
             auto window = new ApplicationWindow(application);
-            window.setTitle("Window");
+            window.setTitle("My GTK App");
 
-            auto box = new Box(Orientation.HORIZONTAL, 0);
-            window.setChild(box);
-
-            auto button = new Button("Press me");
+            auto button = new Button("Press me!");
             button.setMarginTop(12);
             button.setMarginBottom(12);
             button.setMarginStart(12);
             button.setMarginEnd(12);
-            button.addOnClicked((Button _) {
-                    button.setLabel("Hello");
+            button.addOnClicked((Button button) {
+                    button.setLabel("Hello World!");
                 });
-            box.append(button);
-
+            window.setChild(button);
             window.show();
         });
     return application.run(args);
